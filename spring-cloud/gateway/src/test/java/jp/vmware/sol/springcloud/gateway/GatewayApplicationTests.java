a4bc58d4ac47a43d5e30c40e49929eb7bec928df
Shuichi Machida <shuichi.machida@gmail.com>
2020-03-03 15:56:14 +0900

    Spring Cloud Gateway

--- spring-cloud/gateway/src/test/java/jp/vmware/sol/springcloud/gateway/GatewayApplicationTests.java
+++ spring-cloud/gateway/src/test/java/jp/vmware/sol/springcloud/gateway/GatewayApplicationTests.java
@@ -1,9 +1,14 @@
 package jp.vmware.sol.springcloud.gateway;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"eureka.client.enabled=false"})
 class GatewayApplicationTests {
 
     @Test
