ac2cd88ba7b9633a73f92b2b5c7f62b8d2fbd9da
Shuichi Machida <shuichi.machida@gmail.com>
2020-03-25 00:48:33 +0900

    Replacing some Spring Cloud services with kubernetes features

--- scripts/k8s/94-k8s-prefetch-images.sh
+++ scripts/k8s/94-k8s-prefetch-images.sh
@@ -5,6 +5,7 @@ echo "pulling images ..."
 
 docker pull mysql:5.7
 docker pull mongo:3.6.9
+docker pull mongo:latest
 docker pull rabbitmq:3.7.8-management
 docker pull openzipkin/zipkin:2.12.9
 docker pull openjdk:latest
